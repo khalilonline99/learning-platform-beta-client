abf72cfdfec85a4a368af50d15d240aae9f2cb42
khalilonline99 <khalilonline99@gmail.com>
2022-11-01 22:14:45 +0600

    nav bar initiated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
   }, [])
 
   return (
-    <div className='App'> 
+    <div className='App'>
+      
         <RouterProvider router={ router }></RouterProvider>
     </div>
   );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import { CiSun } from 'react-icons/ci';
+import { RiMoonClearFill } from 'react-icons/ri';
 
 const Header = () => {
     return (
 
-        <div className='nav-container'>
-            <h2> <Link to='/'> Mentor Hero</Link></h2>
-            <div>
-            <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/courses'>Courses</Link>
-            <Link to='/faq'>FAQ</Link>
-            <Link to='/blog'>Blog</Link> 
-            <input data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" type="checkbox" className="toggle"/>
-            
+        <div className='grid grid-cols-3 gap-4 py-5'>
+            <h2 className='text-lg font-bold tracking-wide font-serif'> <Link to='/'> Mentor Hero</Link></h2>
 
-            
-        </nav>
+            <div className='nav-middle'>
+                <div>
+                <Link to='/'>Home</Link>
+                <Link to='/courses'>Courses</Link>
+                <Link to='/faq'>FAQ</Link>
+                <Link to='/blog'>Blog</Link>
+                </div>
+
+                <div className='toggle-theme'>
+                    <CiSun />
+                    <input data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" type="checkbox" className="toggle" />
+                    <RiMoonClearFill />
+                </div>
             </div>
+
+
+            <div className='btn-user-auth'>
+                <button className="btn btn-sm btn-primary">Login</button>
+                <button className="btn btn-sm">Register</button>
+            </div>
+
+
         </div>
 
     );
